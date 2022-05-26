b20ed60fbe7357509b1afc5f388f04671e8c9752
Ayrton Juarez <ayrtonjuarez90@gmail.com>
2022-05-26 12:59:13 -0300

    url

--- client/state.ts
+++ client/state.ts
@@ -1,5 +1,5 @@
-// const API_BASE_URL = "https://apx-m6.herokuapp.com";
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://apx-m6.herokuapp.com";
+// const API_BASE_URL = "http://localhost:3000";
 import { getDatabase, ref, onValue, update } from "firebase/database";
 import { rtdb } from "../server/rtdb";
 
